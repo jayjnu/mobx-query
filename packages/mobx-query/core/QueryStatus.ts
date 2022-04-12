278ad858d12d8d70492d6a1951d13d7166badf64
jayjnu <doublejnu@gmail.com>
2022-04-12 22:29:36 +0900

    feat: implement QueryStatus

---
+++ packages/mobx-query/core/QueryStatus.ts
@@ -0,0 +1,53 @@
+import {makeAutoObservable} from 'mobx';
+
+const status = {
+  idle: 'idle',
+  loading: 'loading',
+  success: 'success',
+  error: 'error'
+} as const;
+
+type QueryStatuses = (typeof status)[keyof typeof status]
+
+export default class QueryStatus {
+  private status: QueryStatuses = status.idle
+
+  get isLoading() {
+    return this.status === status.loading;
+  }
+
+  get isSuccess() {
+    return this.status === status.success;
+  }
+  get isUninitialized() {
+    return this.status === status.idle;
+  }
+
+  get isError() {
+    return this.status === status.error;
+  }
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  private setStatus(status: QueryStatuses) {
+    this.status = status;
+  }
+
+  protected idle() {
+    this.setStatus(status.idle);
+  }
+
+  protected success() {
+    this.setStatus(status.success);
+  }
+
+  protected load() {
+    this.setStatus(status.loading);
+  }
+
+  protected error() {
+    this.setStatus(status.error);
+  }
+}
